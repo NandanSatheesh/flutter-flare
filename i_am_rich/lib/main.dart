cf38854c7322694919a6e36b77bfa4df32a05d27
NandanSatheesh <nandan.199776@gmail.com>
2019-09-19 12:27:46 +0530

    Centered it!

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Some Text"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am Rich'),
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+        ),
       ),
     ),
   );
