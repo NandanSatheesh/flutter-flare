cf020a7428d3720865f0bd69952239c33e5ba525
NandanSatheesh <nandan.199776@gmail.com>
2019-09-21 15:45:20 +0530

    Adding Fonts

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,17 +11,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 80,
-                height: 500,
-                color: Colors.green,
+              Center(
+                child: CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: NetworkImage(
+                      'https://avatars0.githubusercontent.com/u/22286630?s=460&v=4'),
+                  backgroundColor: Colors.transparent,
+                ),
               ),
-              Container(
-                width: 80,
-                height: 500,
-                color: Colors.blue,
+              Center(
+                child: Text('Hi! I\'m Nandan',
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Center(
+                child: Text('Software Developer',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal[100],
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold)),
               )
             ],
           ),
