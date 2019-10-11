c0011b4d4afca54d235ba9f3584f33b4462058dc
NandanSatheesh <nandan.199776@gmail.com>
2019-10-11 12:00:45 +0530

    Text Decoration

--- rest_with_flutter/lib/app_screens/first.dart
+++ rest_with_flutter/lib/app_screens/first.dart
@@ -1,12 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MyFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      "Hello World",
-      style: TextStyle(fontSize: 40, color: Colors.blue),
-    ));
+        child: Container(
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
+            decoration:
+                BoxDecoration(color: Colors.green, shape: BoxShape.rectangle),
+            child: Text(
+              generateRandomNumber(),
+              style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  fontSize: 40,
+                  color: Colors.blue),
+            )));
+  }
+
+  String generateRandomNumber() {
+    var random = Random();
+    int randomNumber = random.nextInt(100);
+    return "Your Random Number is $randomNumber";
   }
 }
