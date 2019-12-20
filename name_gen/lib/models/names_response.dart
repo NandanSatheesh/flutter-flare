0af1f43f1e54d4f874ba1a3eb17335ee2fc56a17
NandanSatheesh <nandan.199776@gmail.com>
2019-12-21 01:35:22 +0530

    Adding All Files

--- name_gen/lib/models/names_response.dart
+++ name_gen/lib/models/names_response.dart
@@ -1,46 +1,5 @@
 import 'dart:convert';
 
-NamesResponse namesResponseFromJson(String str) =>
-    NamesResponse.fromJson(json.decode(str));
+List<String> namesResponseFromJson(String str) => List<String>.from(json.decode(str).map((x) => x));
 
-String namesResponseToJson(NamesResponse data) => json.encode(data.toJson());
-
-class NamesResponse {
-  Data data;
-  int status;
-  String message;
-
-  NamesResponse({
-    this.data,
-    this.status,
-    this.message,
-  });
-
-  factory NamesResponse.fromJson(Map<String, dynamic> json) => NamesResponse(
-        data: Data.fromJson(json["data"]),
-        status: json["status"],
-        message: json["message"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "data": data.toJson(),
-        "status": status,
-        "message": message,
-      };
-}
-
-class Data {
-  List<String> names;
-
-  Data({
-    this.names,
-  });
-
-  factory Data.fromJson(Map<String, dynamic> json) => Data(
-        names: List<String>.from(json["names"].map((x) => x)),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "names": List<dynamic>.from(names.map((x) => x)),
-      };
-}
+String namesResponseToJson(List<String> data) => json.encode(List<dynamic>.from(data.map((x) => x)));
---
+++ text_field_validations/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'Enter',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Something Here'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
